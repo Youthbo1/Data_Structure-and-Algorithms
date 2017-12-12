ff113be8409af8d8336e35c764c874c6a9012ba9
bo <389616868@qq.com>
2017-12-12 23:24:07 +0800

    tt1

--- LeetCode_algorithm_practice/src/test_12_12_链表/No206/Solution.java
+++ LeetCode_algorithm_practice/src/test_12_12_链表/No206/Solution.java
@@ -4,8 +4,34 @@ package test_12_12_链表.No206;
  * \Date: 2017/12/11
  * \
  * \Description:
- * \
+ * \/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
+
 public class Solution {
 
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
 }
