9fceb1d333ff66f1ffc826cbe073d1d9d4d0d862
bo <389616868@qq.com>
2017-12-12 23:34:47 +0800

    tt1

--- LeetCode_algorithm_practice/src/test_12_12_链表/No206/Solution.java
+++ LeetCode_algorithm_practice/src/test_12_12_链表/No206/Solution.java
@@ -34,4 +34,36 @@ public class Solution {
 
         return pre;//当前
     }
+
+    public ListNode create(int[] arr,int n ){
+        if(n==0)
+            return null;
+
+        ListNode head=new ListNode(arr[0]);
+
+        ListNode curNode=head;
+        for (int i = 1; i <n ; i++) {
+            curNode.next=new ListNode(arr[i]);
+            curNode=curNode.next;
+        }
+
+        return head;
+    }
+
+    public void printList(ListNode head){
+        ListNode curNode=head;
+        while (curNode!=null){
+            System.out.print(curNode.val+"->");
+            curNode=curNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        int n=a.length;
+
+        ListNode head=new Solution().create(a,n);
+        new Solution().printList(head);
+    }
 }
