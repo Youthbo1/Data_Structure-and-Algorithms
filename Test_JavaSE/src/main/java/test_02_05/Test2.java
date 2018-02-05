e40691513ab124e93df916148aed254b32c15cf6
lichao <663622511@qq.com>
2018-02-05 15:40:31 +0800

    2-05

---
+++ Test_JavaSE/src/main/java/test_02_05/Test2.java
@@ -0,0 +1,32 @@
+package test_02_05;
+
+import sun.rmi.runtime.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Test2 {
+    //static Logger log = Logger.getLogger(Test2.class);
+    public static void main(String[] args) {
+       // List arrayList = new ArrayList();
+//        List<String> arrayList = new ArrayList<String>();
+//        arrayList.add("aaaa");
+//       // arrayList.add(100);
+//
+//        for(int i = 0; i< arrayList.size();i++){
+//            String item = (String)arrayList.get(i);
+//            log.info("泛型测试","item = " + item);
+//        }
+
+//        List<String> stringArrayList = new ArrayList<String>();
+//        List<Integer> integerArrayList = new ArrayList<Integer>();
+//
+//        Class classStringArrayList = stringArrayList.getClass();
+//        Class classIntegerArrayList = integerArrayList.getClass();
+//
+//        if(classStringArrayList.equals(classIntegerArrayList)){
+//            System.out.println("泛型测试","类型相同");
+        }
+    }
+}
